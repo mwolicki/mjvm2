e57a4dd1e31cb6840065b6833805c455c5cdcf4b
Marcin Wolicki <marcin.wolicki@gmail.com>
2020-01-05 17:24:15 -0800

    copied fable example

---
+++ Program.fs
@@ -0,0 +1,10 @@
+﻿open Parser
+
+["HelloWorld$INestedA.class"; "HelloWorld$INestedB.class"; "HelloWorld.class"; "HelloWorld$NestedA.class"; "HelloWorld$NestedB.class"; "module-info.class"]
+|> List.iter(fun fileName ->
+    "/Users/kevin/_projects/java/" + fileName
+    |> readFile
+    |> parseHeader
+    |> Option.map(fun x -> DomainEnricher.transform x.Result)
+    |> printfn "result for %s\n=========================\n%A" fileName)
+
